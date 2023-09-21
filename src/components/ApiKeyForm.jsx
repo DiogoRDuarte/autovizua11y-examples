f8dc9246254b093108474acb731bdbe472f1221c
Diogo R. Duarte <diogorduarte00@gmail.com>
2023-09-21 12:24:42 +0100

    Changed the message in the starting screen to inform the key is not kept

--- src/components/ApiKeyForm.jsx
+++ src/components/ApiKeyForm.jsx
@@ -1,50 +1,52 @@
 import React, { useState } from "react";
 const ApiKeyForm = ({ apiKey, setApiKey, isValid, setIsValid }) => {
-	const [submitStatus, setSubmitStatus] = useState(true);
-	const handleSubmit = (e) => {
-		e.preventDefault();
-		if (isValidApiKey(apiKey)) {
-			setIsValid(true);
-		} else {
-			setIsValid(false);
-		}
-	};
+  const [submitStatus, setSubmitStatus] = useState(true);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (isValidApiKey(apiKey)) {
+      setIsValid(true);
+    } else {
+      setIsValid(false);
+    }
+  };
 
-	const handleChanges = (e) => {
-		e.preventDefault();
-		setApiKey(e.target.value);
-		if (isValidApiKey(e.target.value)) {
-			setSubmitStatus(false);
-		} else {
-			setSubmitStatus(true);
-		}
-	};
-	const isValidApiKey = (key) => {
-		const apiKeyPattern = /^sk-[a-zA-Z0-9]{48}$/;
-		return apiKeyPattern.test(key);
-	};
-	return (
-		<form onSubmit={handleSubmit}>
-			{" "}
-			<p>Please provide an OpenAI API key</p>{" "}
-			<label>
-				{" "}
-				API Key: <input
-					type="text"
-					placeholder="sk-..."
-					value={apiKey}
-					onChange={handleChanges}
-				/>{" "}
-			</label>{" "}
-			<button type="submit" disabled={submitStatus}>
-				Submit
-			</button>{" "}
-			{submitStatus && apiKey.length > 0 ? (
-				<p style={{ color: "red" }}>
-					Invalid API key format. It should be 48 characters long and start with sk-
-				</p>
-			) : null}{" "}
-		</form>
-	);
+  const handleChanges = (e) => {
+    e.preventDefault();
+    setApiKey(e.target.value);
+    if (isValidApiKey(e.target.value)) {
+      setSubmitStatus(false);
+    } else {
+      setSubmitStatus(true);
+    }
+  };
+  const isValidApiKey = (key) => {
+    const apiKeyPattern = /^sk-[a-zA-Z0-9]{48}$/;
+    return apiKeyPattern.test(key);
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      {" "}
+      <p>Please provide an OpenAI API key (the key is not saved)</p>{" "}
+      <label>
+        {" "}
+        API Key:{" "}
+        <input
+          type="text"
+          placeholder="sk-..."
+          value={apiKey}
+          onChange={handleChanges}
+        />{" "}
+      </label>{" "}
+      <button type="submit" disabled={submitStatus}>
+        Submit
+      </button>{" "}
+      {submitStatus && apiKey.length > 0 ? (
+        <p style={{ color: "red" }}>
+          Invalid API key format. It should be 48 characters long and start with
+          sk-
+        </p>
+      ) : null}{" "}
+    </form>
+  );
 };
 export default ApiKeyForm;
